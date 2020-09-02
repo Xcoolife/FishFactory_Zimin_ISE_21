d33f068844bc3f0ac5b78a747e54b802361851ce
Xcoolife <zimin.pl22@gmail.com>
2020-09-02 13:58:48 +0400

    fix

--- FishFactoryDatabaseImplement/Migrations/20200902090919_newDB.Designer.cs
+++ FishFactoryDatabaseImplement/Migrations/20200902090919_newDB.Designer.cs
@@ -10,8 +10,8 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace FishFactoryDatabaseImplement.Migrations
 {
     [DbContext(typeof(FishFactoryDatabase))]
-    [Migration("20200901212052_FourthMigration")]
-    partial class FourthMigration
+    [Migration("20200902090919_newDB")]
+    partial class newDB
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
